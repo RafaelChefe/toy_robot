d863866dfad837ed9497f2593bcd4ec231dd6827
Rafael Santos <estradeiro@gmail.com>
2015-09-13 15:26:51 -0300

    add report command

---
+++ spec/report_command_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'report_command'
+require 'position'
+require 'robot'
+require 'table'
+
+describe ReportCommand do
+
+  describe '#execute' do
+
+    let(:robot) { Robot.new }
+    let(:table) { Table.new(5, 5) }
+
+    context 'with valid position' do
+      let!(:position) { Position.new(1, 1, 'N') }
+
+      it 'prints report to stdout' do
+        report_command = ReportCommand.new(robot)
+
+        robot.place(table, position)
+
+        expected_report = "1, 1, N\n"
+
+        expect { ReportCommand.new(robot).execute }.to output(expected_report).to_stdout
+      end
+    end
+
+    context 'with invalid position' do
+      let!(:position) { Position.new(-1, 1, 'N') }
+
+      it 'prints report to stdout' do
+        report_command = ReportCommand.new(robot)
+
+        robot.place(table, position)
+
+        expected_report = "robot not in place\n"
+
+        expect { ReportCommand.new(robot).execute }.to output(expected_report).to_stdout
+      end
+    end
+  end
+end
