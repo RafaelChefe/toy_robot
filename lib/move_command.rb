b4120d39ca3da6a30637f8feb9eba600abb9f9ab
Rafael Santos <estradeiro@gmail.com>
2015-09-14 19:56:39 -0300

    refactor move command, add validation

--- lib/move_command.rb
+++ lib/move_command.rb
@@ -1,29 +1,33 @@
 class MoveCommand
 
-  def initialize(robot)
+  def initialize(robot, table)
     @robot = robot
+    @table = table
   end
 
   def execute
     unless @robot.not_placed?
-      case @robot.current_position.direction
-      when 'W'
-        @robot.current_position = Position.new(@robot.current_position.x - 1,
-                                               @robot.current_position.y,
-                                               @robot.current_position.direction)
-      when 'E'
-        @robot.current_position = Position.new(@robot.current_position.x + 1,
-                                               @robot.current_position.y,
-                                               @robot.current_position.direction)
-      when 'N'
-        @robot.current_position = Position.new(@robot.current_position.x,
-                                               @robot.current_position.y + 1,
-                                               @robot.current_position.direction)
-      when 'S'
-        @robot.current_position = Position.new(@robot.current_position.x,
-                                               @robot.current_position.y - 1,
-                                               @robot.current_position.direction)
-      end
+      new_position =
+        case @robot.current_position.direction
+        when 'W'
+          Position.new(@robot.current_position.x - 1,
+                       @robot.current_position.y,
+                       @robot.current_position.direction)
+        when 'E'
+          Position.new(@robot.current_position.x + 1,
+                       @robot.current_position.y,
+                       @robot.current_position.direction)
+        when 'N'
+          Position.new(@robot.current_position.x,
+                       @robot.current_position.y + 1,
+                       @robot.current_position.direction)
+        when 'S'
+          Position.new(@robot.current_position.x,
+                       @robot.current_position.y - 1,
+                       @robot.current_position.direction)
+        end
+
+      @robot.current_position = new_position if @table.position_is_valid?(new_position)
     end
   end
 end
--- spec/move_command_spec.rb
+++ spec/move_command_spec.rb
@@ -7,7 +7,7 @@ describe MoveCommand do
 
     let(:robot) { Robot.new }
     let(:table) { Table.new(5, 5) }
-    let(:move_command) { MoveCommand.new(robot) }
+    let(:move_command) { MoveCommand.new(robot, table) }
 
     it 'decreases X when robot is facing WEST' do
       position = Position.new(1, 1, 'W')
@@ -47,6 +47,16 @@ describe MoveCommand do
       move_command.execute
 
       expect(robot.report).to eq('position report: x == 1, y == 0, direction == S')
+    end
+
+    it 'does nothing when position is invalid' do
+      position = Position.new(4, 4, 'N')
+
+      robot.place(table, position)
+
+      move_command.execute
+
+      expect(robot.report).to eq('position report: x == 4, y == 4, direction == N')
     end
   end
 end
