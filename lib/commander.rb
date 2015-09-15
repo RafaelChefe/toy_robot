094b9ab79e52d5739d0b64c16d14cecaf9c0d7bc
Rafael Santos <estradeiro@gmail.com>
2015-09-14 21:58:52 -0300

    add command parsing class

---
+++ lib/commander.rb
@@ -0,0 +1,24 @@
+class Commander
+
+  def initialize(robot, table)
+    @robot = robot
+    @table = table
+  end
+
+  def parse(command)
+    if command =~ /PLACE\s*\d*,\s*\d*,\s*[NSEW]/
+      command, x, y, direction = command.delete(',').split
+
+      PlaceCommand.new(@robot, @table, Position.new(x.to_i, y.to_i, direction))
+    elsif command =~ /MOVE/
+      MoveCommand.new(@robot, @table)
+    elsif command =~ /LEFT/
+      LeftCommand.new(@robot)
+    elsif command =~ /RIGHT/
+      RightCommand.new(@robot)
+    elsif command =~ /REPORT/
+      ReportCommand.new(@robot)
+    end
+  end
+
+end
