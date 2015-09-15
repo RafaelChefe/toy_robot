c7da6294dcb5f66e87c93cb77399acae70d44bed
Rafael Santos <estradeiro@gmail.com>
2015-09-14 21:58:52 -0300

    add command parsing class

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require_relative 'lib/commander'
 require_relative 'lib/table'
 require_relative 'lib/robot'
 require_relative 'lib/position'
@@ -9,22 +10,18 @@ require_relative 'lib/report_command'
 
 table = Table.new(5, 5)
 robot = Robot.new
+commander = Commander.new(robot, table)
+
+puts "welcome to the toy robot simulator!"
 
 loop do
-  puts "input command: "
-  command = gets
+  puts "input command (EXIT to quit): "
 
-  if command =~ /PLACE\s*\d*,\s*\d*,\s*[NSEW]/
-    command, x, y, direction = command.delete(',').split
+  input = gets
 
-    PlaceCommand.new(robot, table, Position.new(x.to_i, y.to_i, direction)).execute
-  elsif command =~ /MOVE/
-    MoveCommand.new(robot, table).execute
-  elsif command =~ /LEFT/
-    LeftCommand.new(robot).execute
-  elsif command =~ /RIGHT/
-    RightCommand.new(robot).execute
-  elsif command =~ /REPORT/
-    ReportCommand.new(robot).execute
-  end
+  command = commander.parse(input)
+
+  command.execute unless command.nil?
+
+  break if input =~ /exit/i
 end
---
+++ spec/commander_spec.rb
@@ -0,0 +1,101 @@
+require 'spec_helper'
+require 'commander'
+require 'left_command'
+require 'move_command'
+require 'place_command'
+require 'right_command'
+require 'report_command'
+require 'position'
+require 'robot'
+require 'table'
+
+describe Commander do
+
+  let(:robot) { Robot.new }
+  let(:table) { Table.new(5, 5) }
+  let(:commander) { Commander.new(robot, table) }
+
+  describe '#parse' do
+    context 'PLACE command' do
+      input = 'PLACE 1, 1, N'
+
+      it 'creates command with the correct arguments' do
+        position = instance_double('Position', x: 1, y: 1, direction: 'N')
+
+        expect(Position).to receive(:new).with(1, 1, 'N').and_return(position)
+
+        expect(PlaceCommand).to receive(:new).with(robot, table, position)
+
+        commander.parse(input)
+      end
+
+      it 'returns command' do
+        expect(commander.parse(input)).to be_a PlaceCommand
+      end
+    end
+
+    context 'MOVE command' do
+      input = 'MOVE'
+
+      it 'creates command with the correct arguments' do
+        expect(MoveCommand).to receive(:new).with(robot, table)
+
+        commander.parse(input)
+      end
+
+      it 'returns command' do
+        expect(commander.parse(input)).to be_a MoveCommand
+      end
+    end
+
+    context 'LEFT command' do
+      input = 'LEFT'
+
+      it 'creates command with the correct arguments' do
+        expect(LeftCommand).to receive(:new).with(robot)
+
+        commander.parse(input)
+      end
+
+      it 'returns command' do
+        expect(commander.parse(input)).to be_a LeftCommand
+      end
+    end
+
+    context 'RIGHT command' do
+      input = 'RIGHT'
+
+      it 'creates command with the correct arguments' do
+        expect(RightCommand).to receive(:new).with(robot)
+
+        commander.parse(input)
+      end
+
+      it 'returns command' do
+        expect(commander.parse(input)).to be_a RightCommand
+      end
+    end
+
+    context 'REPORT command' do
+      input = 'REPORT'
+
+      it 'creates command with the correct arguments' do
+        expect(ReportCommand).to receive(:new).with(robot)
+
+        commander.parse(input)
+      end
+
+      it 'returns command' do
+        expect(commander.parse(input)).to be_a ReportCommand
+      end
+    end
+
+    context 'INVALID command' do
+      it 'returns nil' do
+        input = 'asdfjoaifjçj'
+
+        expect(commander.parse(input)).to be nil
+      end
+    end
+  end
+end
