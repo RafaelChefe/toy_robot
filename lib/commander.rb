5cc8fb0e0eb91525a45075bf4822f618dc2a86f2
Rafael Santos <estradeiro@gmail.com>
2016-06-06 22:58:58 -0300

    Improving command parsing

--- lib/commander.rb
+++ lib/commander.rb
@@ -8,7 +8,7 @@ class Commander
   # parses user input and issues the appropriate commands to the robot
   def parse(command)
     if command =~ /^PLACE\s+\d+\s*,\s*\d+\s*,\s*(NORTH|SOUTH|EAST|WEST)$/
-      command, x, y, direction = command.delete(',').split
+      command, x, y, direction = command.gsub(',', ' ').split
 
       PlaceCommand.new(@robot, @table, Position.new(x.to_i, y.to_i, direction))
     elsif command =~ /^MOVE$/
