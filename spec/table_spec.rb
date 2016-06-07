8d28d9c4967741a7c4fef7172952d65ea61126b1
Rafael Santos <estradeiro@gmail.com>
2016-06-06 22:18:10 -0300

    Refactor table

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -9,43 +9,43 @@ describe Table do
     let(:table) { Table.new(5, 5) }
 
     it 'returns true when x and y are valid' do
-      position = Position.new(2, 3 , 'N')
+      position = Position.new(2, 3 , 'NORTH')
 
       expect(table.position_is_valid?(position)).to be true
     end
 
     it 'returns false when X is too big' do
-      position = Position.new(8, 2, 'N')
+      position = Position.new(8, 2, 'NORTH')
 
       expect(table.position_is_valid?(position)).to be false
     end
 
     it 'returns false when Y is too big' do
-      position = Position.new(2, 8, 'N')
+      position = Position.new(2, 8, 'NORTH')
 
       expect(table.position_is_valid?(position)).to be false
     end
 
     it 'returns false when X and Y are too big' do
-      position = Position.new(8, 8, 'N')
+      position = Position.new(8, 8, 'NORTH')
 
       expect(table.position_is_valid?(position)).to be false
     end
 
     it 'returns false when X is negative' do
-      position = Position.new(-1, 2, 'N')
+      position = Position.new(-1, 2, 'NORTH')
 
       expect(table.position_is_valid?(position)).to be false
     end
 
     it 'returns false when Y is negative' do
-      position = Position.new(2, -1, 'N')
+      position = Position.new(2, -1, 'NORTH')
 
       expect(table.position_is_valid?(position)).to be false
     end
 
     it 'returns false when X and Y are too big' do
-      position = Position.new(-1, -1, 'N')
+      position = Position.new(-1, -1, 'NORTH')
 
       expect(table.position_is_valid?(position)).to be false
     end
