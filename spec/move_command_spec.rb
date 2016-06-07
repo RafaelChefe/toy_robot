00cdca27f19ba7f7227497260f26e960e0d8b385
Rafael Santos <estradeiro@gmail.com>
2016-06-06 22:30:17 -0300

    Refactor move command

--- spec/move_command_spec.rb
+++ spec/move_command_spec.rb
@@ -14,53 +14,53 @@ describe MoveCommand do
     let(:move_command) { MoveCommand.new(robot, table) }
 
     it 'decreases X when robot is facing WEST' do
-      position = Position.new(1, 1, 'W')
+      position = Position.new(1, 1, 'WEST')
 
       PlaceCommand.new(robot, table, position).execute
 
       move_command.execute
 
-      expect(robot.report).to eq('0, 1, W')
+      expect(robot.report).to eq('0, 1, WEST')
     end
 
     it 'increases X when robot is facing EAST' do
-      position = Position.new(1, 1, 'E')
+      position = Position.new(1, 1, 'EAST')
 
       PlaceCommand.new(robot, table, position).execute
 
       move_command.execute
 
-      expect(robot.report).to eq('2, 1, E')
+      expect(robot.report).to eq('2, 1, EAST')
     end
 
     it 'increases Y when robot is facing NORTH' do
-      position = Position.new(1, 1, 'N')
+      position = Position.new(1, 1, 'NORTH')
 
       PlaceCommand.new(robot, table, position).execute
 
       move_command.execute
 
-      expect(robot.report).to eq('1, 2, N')
+      expect(robot.report).to eq('1, 2, NORTH')
     end
 
     it 'decreases Y when robot is facing SOUTH' do
-      position = Position.new(1, 1, 'S')
+      position = Position.new(1, 1, 'SOUTH')
 
       PlaceCommand.new(robot, table, position).execute
 
       move_command.execute
 
-      expect(robot.report).to eq('1, 0, S')
+      expect(robot.report).to eq('1, 0, SOUTH')
     end
 
     it 'does nothing when position is invalid' do
-      position = Position.new(4, 4, 'N')
+      position = Position.new(4, 4, 'NORTH')
 
       PlaceCommand.new(robot, table, position).execute
 
       move_command.execute
 
-      expect(robot.report).to eq('4, 4, N')
+      expect(robot.report).to eq('4, 4, NORTH')
     end
   end
 end
